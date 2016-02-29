1a851573e407bdaed20cec6993af224dae7aed9b
Caleb Stultz <kalub92@gmail.com>
2016-03-01 01:04:47 +0700

    Initial Commit

---
+++ Happy Valley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy Valley
+//
+//  Created by Caleb Stultz on 3/1/16.
+//  Copyright © 2016 Caleb Stultz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
